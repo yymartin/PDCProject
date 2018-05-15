e8c83cf89562c662aaa063006c824746a7b74e14
Yoan Martin <yoan.martin@epfl.ch>
2018-05-15 16:54:23 +0200

    match filter bug corrected
    typo: fs -> Fs

--- getMaxFrequencyFromWav.m
+++ getMaxFrequencyFromWav.m
@@ -2,7 +2,7 @@ function result = getMaxFrequencyFromWav(filename, minband, maxband, desired)
     [y,Fs] = audioread(filename);
     
     ydft = fft(y,Fs);
-    freq = 0:fs/length(ydft):Fs/2;
+    freq = 0:Fs/length(ydft):Fs/2;
     ydft = ydft(minband:maxband);
     
     [~,idx] = max(abs(ydft));
