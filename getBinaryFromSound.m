4e624aae54ca55200269da9f84fd6c310e04f280
Yoan Martin <yoan.martin@epfl.ch>
2018-05-17 17:23:27 +0200

    Add function to detect silence in a signal

--- getBinaryFromSound.m
+++ getBinaryFromSound.m
@@ -1,3 +1,4 @@
+%% Function which recovers a wav file into the corresponding binary
 function getBinaryFromSound(filename,sound_time)
     if(nargin < 2)
         filename = 'sound.wav';
@@ -10,7 +11,7 @@ function getBinaryFromSound(filename,sound_time)
     for i = iter
         temp = y(i:i+(Fs*sound_time));
         frequency = detectMaxFreq(temp,Fs);
-            if (frequency ~= 0) 
+            if (~is_silence(temp,0)) 
                 if (abs(1800-frequency) < abs(frequency-1200))
                     text = strcat(text,'1');
                 else 
@@ -22,6 +23,8 @@ function getBinaryFromSound(filename,sound_time)
     disp(text);
 end
 
+
+%% Function which return the maximum frequency of a signal
 function max_freq = detectMaxFreq(signal,Fs)
     ydft = fft(signal);
     freq = 0:Fs/length(ydft):Fs/2;    
@@ -29,6 +32,18 @@ function max_freq = detectMaxFreq(signal,Fs)
     max_freq = freq(idx);
 end
 
+
+
+%% Function which determines if a signal is a beep or a silence regarding a given threshold
 function silence = is_silence(signal, threshold)
-    %%TODO
+    count_silence = 0;
+    count_noise = 0;
+    for i = 1:length(signal)
+        if(abs(signal(i)) > threshold)
+            count_noise = count_noise + 1;
+        else
+            count_silence = count_silence + 1;
+        end
+    end
+    silence = count_silence > count_noise;
 end
