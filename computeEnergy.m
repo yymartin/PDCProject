0e36bf650826c3545b3359ff3dc228ac58150418
marionkramer <marion.kramer@epfl.ch>
2018-05-21 13:57:57 +0200

    computeEnergy modified to take in input a signal, a frequency and a specific range

--- computeEnergy.m
+++ computeEnergy.m
@@ -1,5 +1,12 @@
-function energy = computeEnergy(signal)
-    F = fft(signal); 
-    pow = F.*conj(F);
-    energy = sum(pow);
+function energy = computeEnergy(signal, fs, rangeFreq)
+    if(nargin < 3)
+        fs = 44100;
+        t = 0:1/fs:1
+        signal = sin(1200*2*pi*t);
+        rangeFreq = [1000 2000];
+    end
+    F = fft(signal);
+    energy = bandpower(F, fs, rangeFreq);
+    %pow = F.*conj(F);
+    %energy = sum(pow);
 end
