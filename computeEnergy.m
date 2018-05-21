98f3a580c46ffe51fc5c7d74d8ce79d11411e575
marionkramer <marion.kramer@epfl.ch>
2018-05-21 14:00:16 +0200

    Add a semi-colon

--- computeEnergy.m
+++ computeEnergy.m
@@ -1,7 +1,7 @@
 function energy = computeEnergy(signal, fs, rangeFreq)
     if(nargin < 3)
         fs = 44100;
-        t = 0:1/fs:1
+        t = 0:1/fs:1;
         signal = sin(1200*2*pi*t);
         rangeFreq = [1000 2000];
     end
