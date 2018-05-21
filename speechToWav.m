a11994ac598728c15eb8b4a099e70ccc33ed6994
marionkramer <marion.kramer@epfl.ch>
2018-05-21 13:24:23 +0200

    SpeechToWav updated for one sound

--- speechToWav.m
+++ speechToWav.m
@@ -2,11 +2,11 @@ function speechToWav()
 
 %CD Quality record
 recObj = audiorecorder(44100, 16, 2);
-
 disp('Start speaking.')
-recordblocking(recObj, 5);
+recordblocking(recObj, 3);
 disp('End of Recording.');
-
+Fs = 44100;
 %Save recorder audio to wave file in same directory
 y = getaudiodata(recObj);
+
 audiowrite('sound.wav',y,44100);
